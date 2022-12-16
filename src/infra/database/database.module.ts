448b2ca8873cdbde012b3b5a71b6abc97c8f938b
Elisio Mualumene <elisiomualumene@gmail.com>
2022-12-16 22:38:49 +0100

    :zap: database module

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
+import { INotificationRepository } from 'src/app/repositories/INotificationRepository';
+import { PrismaNotificationRepository } from './prisma/repositories/PrismaNotificationRepository';
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [
+    PrismaService,
+    {
+      provide: INotificationRepository,
+      useClass: PrismaNotificationRepository,
+    },
+  ],
+  exports: [INotificationRepository],
+})
+export class DatabaseModule {}
