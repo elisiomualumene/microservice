89b9144f9cf16f1975743b66d30acc573e6f6081
Elisio Mualumene <elisiomualumene@gmail.com>
2022-12-16 22:37:27 +0100

    :car: notification class

---
+++ src/app/entities/notification.ts
@@ -0,0 +1,64 @@
+import { Content } from './content';
+import { Replace } from '../../helpers/replace';
+import { randomUUID } from 'crypto';
+
+interface INotificationProps {
+  content: Content;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+  recipientId: string;
+}
+
+export class Notification {
+  private _id: string;
+  private props: INotificationProps;
+
+  constructor(props: Replace<INotificationProps, { createdAt?: Date }>) {
+    this._id = randomUUID();
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
+  }
+
+  public get id() {
+    return this._id;
+  }
+
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+
+  public get recipientId() {
+    return this.props.recipientId;
+  }
+
+  public set content(content: Content) {
+    this.props.content = content;
+  }
+
+  public get content(): Content {
+    return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category() {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date) {
+    this.props.readAt = readAt;
+  }
+
+  public get readAt(): Date {
+    return this.props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.readAt;
+  }
+}
