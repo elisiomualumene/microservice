db502fca8fa3f4466529fb4bd7c6129e449db786
Elisio Mualumene <elisiomualumene@gmail.com>
2022-12-16 22:39:16 +0100

    :zap: prisma notification repository

---
+++ src/infra/database/prisma/repositories/PrismaNotificationRepository.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from './../prisma.service';
+import { Notification } from '../../../../../src/app/entities/notification';
+import { INotificationRepository } from '../../../../app/repositories/INotificationRepository';
+
+@Injectable()
+export class PrismaNotificationRepository implements INotificationRepository {
+  constructor(private prisma: PrismaService) {}
+  async create(notification: Notification): Promise<void> {
+    await this.prisma.notification.create({
+      data: {
+        category: notification.category,
+        content: notification.content.value,
+        recipientId: notification.recipientId,
+        CreatedAt: notification.createdAt,
+        readAt: notification.readAt,
+        id: notification.id,
+      },
+    });
+  }
+}
