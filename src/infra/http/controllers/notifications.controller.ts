5ca4af755132d46da152faf6053352fa9a12b895
Elisio Mualumene <elisiomualumene@gmail.com>
2022-12-16 22:39:42 +0100

    :zap: notification controller

---
+++ src/infra/http/controllers/notifications.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateNotificationBody } from '../dtos/create-notification-body';
+import { SendNotificationUseCase } from '../../../app/UseCases/SendNotificationUsecase';
+
+@Controller('notifications')
+export class NotificationsController {
+  constructor(private SendNotification: SendNotificationUseCase) {}
+  @Post()
+  async createNotification(@Body() body: CreateNotificationBody) {
+    const { content, recipientId, category } = body;
+
+    const { notification } = await this.SendNotification.execute({
+      content,
+      recipientId,
+      category,
+    });
+
+    return { notification };
+  }
+}
