0f48f5c38558e6660dedbe19523cbdfbb3c17b53
Elisio Mualumene <elisiomualumene@gmail.com>
2022-12-20 22:10:35 +0100

    :hammer: main bootstrap kafka

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { KafkaConsumerService } from './infra/messaging/kafka/kafkaComsumer.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const kafkaConsumerService = app.get(KafkaConsumerService);
+  app.connectMicroservice<MicroserviceOptions>({
+    strategy: kafkaConsumerService,
+  });
+  app.startAllMicroservices();
   app.listen(3200);
 }
 
