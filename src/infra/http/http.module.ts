91955c0e7ab594a996b875617f0c0f3cbeb69f08
Elisio Mualumene <elisiomualumene@gmail.com>
2022-12-20 21:07:44 +0100

    http :car: module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,11 +1,25 @@
+import { CancelNotificationUseCase } from './../../app/UseCases/CancelNotification/CancelNotificationUseCase';
 import { Module } from '@nestjs/common';
 import { NotificationsController } from './controllers/notifications.controller';
-import { SendNotificationUseCase } from '../../app/UseCases/SendNotificationUsecase';
+import { SendNotificationUseCase } from '../../app/UseCases/SendNotification/SendNotificationUsecase';
 import { DatabaseModule } from '../database/database.module';
+import { ReadNotificationUseCase } from '../../app/UseCases/ReadNotification/ReadNotificationUseCase';
+import { UnreadNotificationUseCase } from '../../app/UseCases/UnreadNotification/UnreadNotificationUseCase';
+import { CountNotificationUseCase } from '../../app/UseCases/CountNotification/CountNotificstionUseCase';
+import { RecipientNotificationUseCase } from '../../app/UseCases/RecipienNotifications/RecipientNotificationUseCase';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotificationUseCase],
+  providers: [
+    SendNotificationUseCase,
+    CancelNotificationUseCase,
+    ReadNotificationUseCase,
+    UnreadNotificationUseCase,
+    ReadNotificationUseCase,
+    CountNotificationUseCase,
+    ReadNotificationUseCase,
+    RecipientNotificationUseCase,
+  ],
 })
 export class httpModule {}
