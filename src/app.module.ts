84cbc92de3adbc69801523ebb5500d8fb1cae779
Elisio Mualumene <elisiomualumene@gmail.com>
2022-12-20 22:10:22 +0100

    :car: kafka module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './infra/database/database.module';
 import { httpModule } from './infra/http/http.module';
+import { MessagingModule } from './infra/messaging/messaging.module';
 
 @Module({
-  imports: [httpModule, DatabaseModule],
+  imports: [httpModule, DatabaseModule, MessagingModule],
 })
 export class AppModule {}
