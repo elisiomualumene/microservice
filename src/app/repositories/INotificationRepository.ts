1bd54454dcb6da742477ab410a261656fc306013
Elisio Mualumene <elisiomualumene@gmail.com>
2022-12-20 21:04:58 +0100

    :hammer: abstract methods

--- src/app/repositories/INotificationRepository.ts
+++ src/app/repositories/INotificationRepository.ts
@@ -4,6 +4,10 @@ abstract class INotificationRepository {
   abstract create(notification: Notification): Promise<void>;
   abstract findById(notificationId: string): Promise<Notification | null>;
   abstract save(notification: Notification): Promise<void>;
+  abstract countByRecipientId(recipientId: string): Promise<number>;
+  abstract NotificationsByRecipientId(
+    recipientId: string,
+  ): Promise<Notification[]>;
 }
 
 export { INotificationRepository };
